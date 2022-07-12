afa69fc455fa229006d73dfcd233b05b1775e343
Susie <xu.shu1@northeastern.edu>
2022-07-11 19:59:30 -0700

    add evaluation

---
+++ FinalProject/src/Evaluation.java
@@ -0,0 +1,25 @@
+public class Evaluation {
+    public static int[][] getBestMaskPattern(int[][][] matrices) {
+        return new int[][]{};
+    }
+
+    private static int calculatePenaltyScore(int[][] matrix) {
+        return 0;
+    }
+
+    private static int calculatePenaltyRule1(int[][] matrix) {
+        return 0;
+    }
+
+    private static int calculatePenaltyRule2(int[][] matrix) {
+        return 0;
+    }
+
+    private static int calculatePenaltyRule3(int[][] matrix) {
+        return 0;
+    }
+
+    private static int calculatePenaltyRule4(int[][] matrix) {
+        return 0;
+    }
+}
